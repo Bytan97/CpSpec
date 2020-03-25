a5278fceb6ddff70addd035ac3840a4876901712
Bytan97 <enbsfn@gmail.com>
2020-03-25 22:01:49 +0300

    123

---
+++ YelBelt/week 1/tracker/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+// Перечислимый тип для статуса задачи
+enum class TaskStatus {
+  NEW,          // новая
+  IN_PROGRESS,  // в разработке
+  TESTING,      // на тестировании
+  DONE          // завершена
+};
+
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = map<TaskStatus, int>;
+
+class TeamTasks {
+ private:
+  map<string, TasksInfo> person_to_task;
+ public:
+  // Получить статистику по статусам задач конкретного разработчика
+  const TasksInfo &GetPersonTasksInfo(const string &person) const {
+//    if (person_to_task.count(person))
+    return person_to_task.at(person);
+  }
+  // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+  void AddNewTask(const string &person) {
+    person_to_task[person][TaskStatus::NEW] += 1;
+  }
+  // Обновить статусы по данному количеству задач конкретного разработчика,
+  tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+      const string &person, int task_count) {
+    if (person_to_task.count(person) == 0) {
+      return {{}, {}};
+    }
+    TasksInfo Updated, Untouched, current_person = person_to_task[person];
+    for (int intStatus = static_cast<int>(TaskStatus::NEW); intStatus != static_cast<int>(TaskStatus::DONE);
+         ++intStatus) {
+      int current_task_counts = current_person[static_cast<TaskStatus>(intStatus)];
+      if (task_count == 0) {
+        break;
+      }
+      if (current_task_counts < task_count) {
+        current_person.erase(static_cast<TaskStatus>(intStatus));
+        current_person[static_cast<TaskStatus>(intStatus + 1)] += current_task_counts;
+        Updated[static_cast<TaskStatus>(intStatus + 1)] = current_task_counts;
+        task_count -= current_task_counts;
+      } else {
+        current_person[static_cast<TaskStatus>(intStatus)] -= task_count;
+        current_person[static_cast<TaskStatus>(intStatus + 1)] += task_count;
+        Updated[static_cast<TaskStatus>(intStatus + 1)] = task_count;
+        task_count = 0;
+      }
+    }
+    for (int intStatus = static_cast<int>(TaskStatus::NEW); intStatus != static_cast<int>(TaskStatus::DONE);
+         ++intStatus) {
+      Untouched[static_cast<TaskStatus>(intStatus)] =
+          current_person[static_cast<TaskStatus>(intStatus)] - Updated[static_cast<TaskStatus>(intStatus)];
+      if (Updated[static_cast<TaskStatus>(intStatus)] == 0) {
+        Updated.erase(static_cast<TaskStatus>(intStatus));
+      }
+      if (Untouched[static_cast<TaskStatus>(intStatus)] == 0) {
+        Untouched.erase(static_cast<TaskStatus>(intStatus));
+      }
+      if (current_person[static_cast<TaskStatus>(intStatus)] == 0) {
+        current_person.erase(static_cast<TaskStatus>(intStatus));
+      }
+    }
+    if (current_person[TaskStatus::DONE] == 0) {
+      current_person.erase(TaskStatus::DONE);
+    }
+    if (Untouched[TaskStatus::DONE] == 0) {
+      Untouched.erase(TaskStatus::DONE);
+    }
+    if (Updated[TaskStatus::DONE] == 0) {
+      Updated.erase(TaskStatus::DONE);
+    }
+    person_to_task[person] = current_person;
+
+    return {Updated, Untouched};
+  }
+};
+
+int main() {
+  TeamTasks tasks;
+  TasksInfo updated_tasks;
+  TasksInfo untouched_tasks;
+  for (auto i = 0; i < 5; ++i) {
+    tasks.AddNewTask("Lisa");
+  }
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 5); // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 5); // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 1); // 1
+  for (auto i = 0; i < 5; ++i) {
+    tasks.AddNewTask("Lisa");
+  }
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 2); // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 4); // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 5); // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 10); // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 10); // 1
+  tasks.AddNewTask("Lisa");                                                                    // 1
+  tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Lisa", 2);
+
+  return 0;
+}
